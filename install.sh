d896538a0746e4984dd07798b805ba8b1513271a
Stephen Nancekivell <stephennancekivell@gmail.com>
2017-01-02 22:11:16 +1100

    rename folder to match repo

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "Installing Scripts"
-mkdir -p ~/bin/brewupdatenotifier
-curl -s https://raw.githubusercontent.com/stephennancekivell/brew-update-notifier/master/check.sh -o ~/bin/brewupdatenotifier/check.sh
-curl -s https://raw.githubusercontent.com/stephennancekivell/brew-update-notifier/master/do-upgrade.sh -o ~/bin/brewupdatenotifier/do-upgrade.sh
-chmod u+x ~/bin/brewupdatenotifier/*.sh
+mkdir -p ~/bin/brew-update-notifier
+curl -s https://raw.githubusercontent.com/stephennancekivell/brew-update-notifier/master/check.sh -o ~/bin/brew-update-notifier/check.sh
+curl -s https://raw.githubusercontent.com/stephennancekivell/brew-update-notifier/master/do-upgrade.sh -o ~/bin/brew-update-notifier/do-upgrade.sh
+chmod u+x ~/bin/brew-update-notifier/*.sh
 echo "Installing Dependency"
 brew install terminal-notifier
 
 echo "Adding Crontab to run every hour"
-(crontab -l ; echo "0 * * * * ~/bin/brewupdatenotifier/check.sh > /dev/null") | sort - | uniq - | crontab -
+(crontab -l ; echo "0 * * * * ~/bin/brew-update-notifier/check.sh > /dev/null") | sort - | uniq - | crontab -
