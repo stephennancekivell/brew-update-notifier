c2b1d3c7322c7597ca79c1acf9f8089980b3e8f7
Stephen Nancekivell <stephen.nancekivell@gmail.com>
2017-06-05 09:16:16 +1000

    dont mail crontab errors. like timeouts.

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ echo "Installing Dependency"
 brew install terminal-notifier
 
 echo "Adding Crontab to run every hour"
-(crontab -l ; echo "0 * * * * ~/bin/brew-update-notifier/check.sh > /dev/null") | sort - | uniq - | crontab -
+(crontab -l ; echo "0 * * * * ~/bin/brew-update-notifier/check.sh > /dev/null 2>&1") | sort - | uniq - | crontab -
